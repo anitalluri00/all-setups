53b3718f5df4f361c8ee362f9b5b32628b36b6df
Anirudh Talluri <168444331+anitalluri00@users.noreply.github.com>
2026-02-15 11:45:28 +0530

    Update EKS.sh

--- EKS.sh
+++ EKS.sh
@@ -1,21 +1,13 @@
 #!/bin/bash
 
-# ==================================
-# AWS EKS Setup Script (Amazon Linux 2023)
-# kernel-6.1 / kernel-6.12
-# ==================================
-
+# AWS EKS Setup Script
 set -e
-
 echo "Updating system..."
 sudo dnf update -y --allowerasing --best --skip-broken
-
 echo "Installing dependencies..."
 sudo dnf install -y unzip curl tar git --allowerasing --best --skip-broken
 
-# ----------------------------------
-# Install AWS CLI v2 (if missing)
-# ----------------------------------
+# Install AWS CLI v2
 if ! command -v aws &> /dev/null
 then
   echo "Installing AWS CLI..."
@@ -23,51 +15,37 @@ then
   unzip -oq awscliv2.zip
   sudo ./aws/install
 fi
-
 echo ">>> Run 'aws configure' manually if not configured"
 
-# ----------------------------------
 # Install kubectl v1.32.0
-# ----------------------------------
 echo "Installing kubectl..."
 curl -LO "https://dl.k8s.io/release/v1.32.0/bin/linux/amd64/kubectl"
 chmod +x kubectl
 sudo mv -f kubectl /usr/local/bin/
-
 kubectl version --client
 
-# ----------------------------------
-# Install eksctl (latest)
-# ----------------------------------
+# Install eksctl
 echo "Installing eksctl..."
 curl --silent --location \
 "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_Linux_amd64.tar.gz" \
 | tar xz -C /tmp
-
 sudo mv -f /tmp/eksctl /usr/local/bin
-
 eksctl version
 
-# ----------------------------------
 # Create EKS Cluster
-# ----------------------------------
 eksctl create cluster \
   --name=kscluster \
   --region=us-east-1 \
   --zones=us-east-1a,us-east-1b \
   --without-nodegroup
 
-# ----------------------------------
 # Associate IAM OIDC Provider
-# ----------------------------------
 eksctl utils associate-iam-oidc-provider \
   --cluster kscluster \
   --region us-east-1 \
   --approve
 
-# ----------------------------------
 # Create Nodegroup
-# ----------------------------------
 eksctl create nodegroup \
   --cluster=kscluster \
   --region=us-east-1 \
@@ -85,5 +63,4 @@ eksctl create nodegroup \
   --full-ecr-access \
   --alb-ingress-access \
   --appmesh-access
-
 echo "EKS Setup Completed!"
