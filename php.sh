d63cafd00a3f5cdb0de263b56d9f885558eee013
Anirudh Talluri <168444331+anitalluri00@users.noreply.github.com>
2026-02-25 07:16:29 +0530

    Create php.sh

---
+++ php.sh
@@ -0,0 +1,15 @@
+sudo apt update -y && sudo apt upgrade -y
+# Install Git & tree
+sudo apt install git tree -y
+# Install Nginx
+sudo apt install nginx -y
+# Install PHP 8.2 + Extensions
+sudo apt install php8.2 php8.2-fpm php8.2-mysql php8.2-xml php8.2-mbstring php8.2-curl php8.2-gd php8.2-bcmath php8.2-zip php8.2-cli unzip git curl -y
+# Install MySQL 8
+sudo apt install mysql-server -y
+# Install NodeJS 18
+curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
+sudo apt install nodejs -y
+# Install Composer
+curl -sS https://getcomposer.org/installer | php
+sudo mv composer.phar /usr/local/bin/composer
