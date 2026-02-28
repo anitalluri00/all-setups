c7213724155b6c13598d1cfa9729ee37653161d7
Anirudh Talluri <168444331+anitalluri00@users.noreply.github.com>
2026-02-28 09:57:38 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,11 +1,22 @@
+#!/bin/bash
 
-#! /bin/bash
+# Update system
+sudo dnf update -y
+
+# Install Git
+sudo dnf install git maven tree -y
+
+# Install Java 17 (Required for Jenkins & Maven)
+sudo dnf install -y java-17-amazon-corretto-devel
+java -version
+
+# Install Jenkins
 sudo wget -O /etc/yum.repos.d/jenkins.repo \
-    https://pkg.jenkins.io/redhat-stable/jenkins.repo
+https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
-sudo yum upgrade
-# Add required dependencies for the jenkins package
-yum install java-17-amazon-corretto jenkins -y
-sudo yum install jenkins -y
-sudo systemctl restart jenkins
-sudo systemctl daemon-reload
+sudo dnf install -y jenkins
+
+# Start and enable Jenkins
+sudo systemctl start jenkins
+sudo systemctl enable jenkins
+sudo systemctl status jenkins
